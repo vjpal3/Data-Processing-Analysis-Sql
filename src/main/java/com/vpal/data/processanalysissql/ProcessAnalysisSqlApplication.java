e63ced385e529b2122aa3f96b022680fa07a5400
vjpal3 <vrishalipal@yahoo.com>
2020-06-04 23:00:28 -0400

    Initial commit

---
+++ src/main/java/com/vpal/data/processanalysissql/ProcessAnalysisSqlApplication.java
@@ -0,0 +1,13 @@
+package com.vpal.data.processanalysissql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessAnalysisSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessAnalysisSqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vpal/data/processanalysissql/ProcessAnalysisSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vpal.data.processanalysissql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessAnalysisSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
